cc04f5e147e2edafbab9a401aa8cc1efd3824785
Sal Gamez <gamezsalvador@gmail.com>
2026-09-10 05:18:01 +0000

    fix: untrack .next build cache

--- .next/server/interception-route-rewrite-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__INTERCEPTION_ROUTE_REWRITE_MANIFEST="[{\"source\":\"/projects/:slug\",\"destination\":\"/(.)projects/:slug\",\"has\":[{\"type\":\"header\",\"key\":\"Next-Url\",\"value\":\"(?:\\\\/(.*))?[\\\\/#\\\\?]?\"}]}]"
